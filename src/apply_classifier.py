2350e684136adba34b6d73ddf63b819685580ec8
gsatas <gsatas7@gmail.com>
2023-03-17 17:21:06 -0400

    chunked and parallelized apply classifier

--- src/apply_classifier.py
+++ src/apply_classifier.py
@@ -8,12 +8,47 @@ def main(args):
     validate_arguments(args)
     print("1. Loading model from: {}".format(args.model_dir))
     model, scaler = load_model(args.model_dir)
-    print("2. Loading data from: {}".format(args.features))
-    df, input_data = get_input_data(args.features, scaler)
-    print("3. Predicting")
-    labels, probs = predict(input_data, model)
-    print("4. Writing results to: {}".format(args.output_dir))
-    write_output(df, labels, probs, args.output_dir)
+    print("2. Classifying data from: {}".format(args.features))
+
+    nlines = sum(1 for row in open(args.features, 'r'))-1
+
+    if args.cores: ncores = args.cores
+    else:
+        import psutil
+        ncores = psutil.cpu_count(logical=False)
+
+    df_reader = pd.read_table(args.features, chunksize=10000)
+    for i, df in enumerate(df_reader):
+        print('\r\t{}/{} variants completed'.format(i*10000, nlines), end = '')
+        first = (i == 0)
+        process_chunk(df, model, scaler, args.output_dir, ncores, first)
+    print('\r\t{}/{} variants completed'.format(nlines, nlines), end = '')
+
+
+def process_chunk(df, model, scaler, output_dir, ncores, first):
+    input_data = scale_data(df, scaler)
+    labels, probs = predict(df, input_data, model, ncores)
+    write_output_chunk(df, labels, probs, output_dir, first)
+
+
+def scale_data(df, scaler):
+    features = [c for c in df.columns if c.startswith('f_')]
+    data = df[features].values
+
+    scaled_data = scaler.transform(data)
+    return scaled_data
+
+def write_output_chunk(df, labels, probs, output_dir, first):
+    df['temp'] = labels
+    df['result'] = df['temp'].apply(lambda x: 'PASS' if x else "ARTIFACT")
+    df['prob'] = probs
+
+    out_df = df[['chrm', 'pos', 'result', 'prob']]
+    out_file = os.path.join(output_dir, 'result.tsv')
+    if first:
+        out_df.to_csv(out_file, sep='\t', index=False)
+    else:
+        out_df.to_csv(out_file, mode = 'a', header = False, sep='\t', index=False)
 
 def add_parser_arguments(parser):
     parser.add_argument(dest='features', type = str, help = '<Required> Input file containing variant features')
@@ -26,24 +61,28 @@ def validate_arguments(args):
 def load_model(model_dir):
     with open(os.path.join(model_dir, 'model.pkl'), 'rb') as f:
         model = pickle.load(f)
+        model.set_params(n_jobs = 10)
 
     with open(os.path.join(model_dir, 'scaler.pkl'), 'rb') as f:
         scaler = pickle.load(f)
     return model, scaler
 
-def get_input_data(input_file, scaler):
-    df = pd.read_table(input_file)
+def predict(df, input_data, model, ncores):
+    scaled_features = ['{}_s'.format(c) for c in df.columns if c.startswith('f_')]
+    df[scaled_features] = input_data
 
-    features = [c for c in df.columns if c.startswith('f_')]
-    data = df[features].values
+    ### Please excuse the following mess
+    ### It parallelizes the model prediction, by breaking the dataframe into chunks that can be predicted on in parallel
+    ### This is done because passing model.predict a large array is way quicker than passing it one row n times
+    nperchunk = len(df)/ncores
+    df['chunk'] = [int(d/nperchunk) for d in df.index]
+    # Do the actual prediction. Returns a series where each element is a list for that chunk (i.e., a list of lists)
+    grouped_results = df.groupby('chunk').parallel_apply(lambda x: model.predict_proba(x[scaled_features].values))
+    # Flatten the list of list, and extract the first value (the probability of it being an artifact)
+    probs = grouped_results.explode().reset_index(drop=True).apply(lambda x: x[0])
 
-    scaled_data = scaler.transform(data)
-    return df, scaled_data
-
-def predict(input_data, model):
-    labels = model.predict(input_data)
-    probs = model.predict_proba(input_data)
-    return labels, probs
+    labels = [p < 0.5 for p in probs]
+    return labels, probs.values
 
 def write_output(df, labels, probs, output_dir):
     df['temp'] = labels
--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 import argparse
+import os
 
-import mixture_model_preprocessing
-import extract_features
-import mixture_model
-import train_classifier
-import apply_classifier
+def setup_module_arguments(parser):
+    import mixture_model_preprocessing
+    import extract_features
+    import mixture_model
+    import train_classifier
+    import apply_classifier
 
-from pandarallel import pandarallel
-
-
-if __name__ == '__main__':
     modes = {
             "extract_features" : extract_features,
             "mm_preprocessing" : mixture_model_preprocessing,
@@ -18,26 +16,26 @@ if __name__ == '__main__':
             "classify": apply_classifier
             }
 
-    parser = argparse.ArgumentParser()
-     # to use. By default, will use all available ')
     subparsers = parser.add_subparsers(dest = 'mode', required = True, \
             help='<Required> Select mode')
 
     for mode in modes:
         subparser = subparsers.add_parser(mode)
         modes[mode].add_parser_arguments(subparser)
-        if mode not in ['train_classifier', 'classify']:
-            subparser.add_argument('--cores', '-j', default = None, type = int, \
-                help = 'Number of workers to use for parallelization. <Default> the number of available cores')
-
+        #if mode not in ['train_classifier', 'classify']:
+        subparser.add_argument('--cores', '-j', default = None, type = int, \
+            help = 'Number of workers to use for parallelization. <Default> the number of available cores')
 
     args = parser.parse_args()
     mode = args.mode
-    if mode not in ['train_classifier', 'classify']:
+
+    if mode not in ['train_classifier']:
+        from pandarallel import pandarallel
+        progress_bar = mode != 'classify'
         if args.cores:
-            pandarallel.initialize(progress_bar=True, nb_workers = args.cores)
+            pandarallel.initialize(progress_bar=progress_bar, nb_workers = args.cores)
         else:
-            pandarallel.initialize(progress_bar=True) #, nb_workers = args.cores)
+            pandarallel.initialize(progress_bar=progress_bar) #, nb_workers = args.cores)
 
     try:
         modes[mode]
@@ -45,3 +43,23 @@ if __name__ == '__main__':
         raise KeyError('Invalid mode provided: {} \nMode must be one of {}'.format(mode, str(modes.keys())))
 
     modes[mode].main(args)
+
+
+def setup_ncores(parser):
+    parser.add_argument('--cores', '-j', default = None, type = int, \
+        help = 'Number of workers to use for parallelization. <Default> the number of available cores')
+    args, arglist = parser.parse_known_args()
+
+    import psutil
+    threads_per_core = psutil.cpu_count() / psutil.cpu_count(logical=False)
+
+    if args.cores:
+        os.environ['OPENBLAS_NUM_THREADS'] = str(args.cores * threads_per_core)
+        os.environ['MKL_NUM_THREADS'] = str(args.cores * threads_per_core)
+
+    return parser, args
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    setup_ncores(parser)
+    setup_module_arguments(parser)
