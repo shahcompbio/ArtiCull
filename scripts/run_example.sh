e6b767a75f60c8f3712497f5f0ac1902444b9fe6
SatasG <SatasG@mskcc.org>
2023-10-10 10:05:38 -0400

    Update run_example.sh
    Added cores option to run_example.sh

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -10,6 +10,7 @@ vcf=/juno/work/shah/isabl_data_lake/analyses/77/41/37741/results/SHAH_H001755_T0
 bam=/juno/work/shah/isabl_data_lake/analyses/67/51/36751/results/SHAH_H001755_T01_01_DLP01_all_cells_bulk.bam
 output_dir=$code_dir/../example
 log_file=$output_dir/classification.log
+cores=8
 echo $log_file
 
-bash $code_dir/run_classify.sh $vcf $output_dir $bam $log_file $map_file
+bash $code_dir/run_classify.sh $vcf $output_dir $bam $log_file $map_file $cores
